081503731d11203922aa4f091b3555777ef06222
Marat1206 <marikzhekov@gmail.com>
2021-06-07 23:16:52 +0300

    init

---
+++ js/switch.js
@@ -0,0 +1,18 @@
+let num = +prompt('Виддите цифру от 1 до 4');
+
+switch (num){
+    case 1:
+        alert('Зима');
+        break;
+    case 2:
+        alert('Весна');
+        break;
+    case 3:
+        alert('Лето');
+        break;
+    case 4:
+        alert('Осень');
+        break;
+    default:
+        alert('Нет таких значений')
+}
